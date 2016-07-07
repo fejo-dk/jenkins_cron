f947c269f155664a09b1e6658ce25421e7577d8f
Dirk Breuer <dirk.breuer@gmail.com>
2016-07-07 15:53:40 +0200

    To assign jobs to jenkins nodes they need a label

--- lib/jenkins_cron/job.rb
+++ lib/jenkins_cron/job.rb
@@ -1,6 +1,6 @@
 module JenkinsCron
   class Job < Whenever::Job
-    attr_reader :group
+    attr_reader :group, :label
 
     def initialize(options = {})
       super
@@ -8,6 +8,7 @@ module JenkinsCron
       @options[:output] = ''
       @group = @options[:group].try(:to_sym) || nil
       @name  = @options[:name]  || raise(ArgumentError.new("A name is required to create a job at Jenkins!"))
+      @label = @options.fetch(:label, nil)
     end
 
     def name
